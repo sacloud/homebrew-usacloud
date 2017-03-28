513f62e6fe184bf7b1d6d8fe40db894ed2aada4a
febc.yamamoto <yamamoto.febc@gmail.com>
2017-03-28 18:23:22 +0900

    v0.0.5

---
+++ usacloud.rb
@@ -0,0 +1,33 @@
+class Usacloud < Formula
+
+  usacloud_version = "0.0.5"
+  sha256_src = "7d1c5bf9b8573d54a84e64ff0d2c06842f2b8ae5f99787da72260c27f1ad1fbd"
+  sha256_bash_completion = "e4eba9bed59018176a39b8e33ea97c066ebd7344b80c2205920261e55498de45"
+
+  desc "Unofficial 'sacloud' - CLI client of the SakuraCloud"
+  homepage "https://github.com/sacloud/usacloud"
+  url "https://github.com/sacloud/usacloud/releases/download/v#{usacloud_version}/usacloud_darwin-amd64.zip"
+  sha256 sha256_src
+  head "https://github.com/sacloud/usacloud.git"
+  version usacloud_version
+
+  option "without-completions", "Disable bash completions"
+  resource "bash_completion" do
+    url "https://usacloud.b.sakurastorage.jp/contrib/completion/bash/usacloud"
+    sha256 sha256_bash_completion
+  end
+
+  def install
+    bin.install "usacloud"
+    if build.with? "completions"
+      resource("bash_completion").stage {
+        bash_completion.install "usacloud"
+      }
+    end
+
+  end
+
+  test do
+    assert_match "SAKURACLOUD_ACCESS_TOKEN", shell_output("usacloud --help")
+  end
+end
