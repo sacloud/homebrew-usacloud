d7e7474de954e64a92e6a91225116e524eb947ac
usacloud <yamamoto.febc@gmail.com>
2017-11-03 01:55:32 +0000

    v0.3.1

--- usacloud.rb
+++ usacloud.rb
@@ -1,8 +1,8 @@
 class Usacloud < Formula
 
-  usacloud_version = "0.3.0"
-  sha256_src_darwin = "73ce0418edd6c6b3e291973ac31d0ac25b342056ebc325cf7cfbec62ed50c45d"
-  sha256_src_linux = "4f77897bfcbc72e5d062519b4a367f70fedb6d8d692980e3dd2742c431372a8d"
+  usacloud_version = "0.3.1"
+  sha256_src_darwin = "3c6b7ea9def03f85530d22911b4dba3dd7fe8a5376a2ee1004c1a315cd234e5a"
+  sha256_src_linux = "b75b2b1dc346ba320f1f9a5716b18733e8f3d6674c5754717752f867b2513b46"
   sha256_bash_completion = "3b2b3e6f3136a842659939f4055e7c98ab6fc24d0ca3aa495f25e2db46df7ee1"
 
   desc "Unofficial 'sacloud' - CLI client of the SakuraCloud"
