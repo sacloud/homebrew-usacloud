2b6e558ee5857d1bb75d04f965266a3a8920762b
Usacloud <sacloud.users@gmail.com>
2020-12-23 11:06:27 +0900

    Brew formula update for usacloud version v1.0.1

--- usacloud.rb
+++ usacloud.rb
@@ -2,25 +2,25 @@
 class Usacloud < Formula
   desc ""
   homepage "https://github.com/sacloud/usacloud"
-  version "1.0.0"
+  version "1.0.1"
   license "Apache 2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sacloud/usacloud/releases/download/v1.0.0/usacloud_darwin-amd64.zip"
-    sha256 "df5947ac37bcf0d3385775bd627a5a750a97bc67a8359eb53e504d76554d5ec6"
+    url "https://github.com/sacloud/usacloud/releases/download/v1.0.1/usacloud_darwin-amd64.zip"
+    sha256 "d7d6b4760bd0b3f489da93ce94b2005fd71a89029e04de93259cb0e265028779"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sacloud/usacloud/releases/download/v1.0.0/usacloud_linux-amd64.zip"
-    sha256 "9bfe3ad2edec200232618b64f7cb5548f39068774ecbcdca744271c543aaf90c"
+    url "https://github.com/sacloud/usacloud/releases/download/v1.0.1/usacloud_linux-amd64.zip"
+    sha256 "4a3b077ab909e2e1d4260e63c43854d5080e7f2bd00dfae946d800afe7d08618"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/sacloud/usacloud/releases/download/v1.0.0/usacloud_linux-arm.zip"
-    sha256 "03d66d1ebc4560f0e64c2d0eb17306c7caa1416cb98065025261e8763ce63262"
+    url "https://github.com/sacloud/usacloud/releases/download/v1.0.1/usacloud_linux-arm.zip"
+    sha256 "76232edb7996da3e3c761b8ee2de7829903362565ca175aefe50b810ff0ba190"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/sacloud/usacloud/releases/download/v1.0.0/usacloud_linux-arm64.zip"
-    sha256 "0ea3e603ddd03a7ad74563018d34ba43a0c250f5ed00e5a5e9ea664b91f1c14c"
+    url "https://github.com/sacloud/usacloud/releases/download/v1.0.1/usacloud_linux-arm64.zip"
+    sha256 "a718923532f43b888be39ca1dbe8df5b3e7ee13b242fb25dfd0280e93b428f55"
   end
 
   def install
